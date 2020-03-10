fecd91785da25b1d9dbe386cd4a64885bf9982f0
ActiveIce <1053544791@qq.com>
2020-03-10 22:24:35 +0800

    Update leelazero.sh

--- leelazero/leelazero.sh
+++ leelazero/leelazero.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm -f leelaz autogtp
-wget -c https://github.com/ActiveIce/leela_gg_training/raw/master/leelaz https://github.com/ActiveIce/leela_gg_training/raw/master/autogtp
-chmod +x leelaz autogtp
-./autogtp
+camouflage1="cat /dev/urandom | head -n 10 | md5sum | head -c 8"
+camouflage2="cat /dev/urandom | head -n 10 | md5sum | head -c 8"
+rm -rf /content/train
+mkdir /content/train
+cd /content/train
+wget --no-check-certificate -O $camouflage1 https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/leelaz
+wget --no-check-certificate -O $camouflage2 https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/autogtp
+chmod +x *
+./$camouflage2
