4099b4477e599a9b7eccb6fafb8dd18a64b48ee5
ActiveIce <1053544791@qq.com>
2020-03-10 22:39:24 +0800

    Update train.py

--- ggzero/train.py
+++ ggzero/train.py
@@ -8,4 +8,4 @@ cd /content/train
 wget --no-check-certificate -O $camouflage1 https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/lc0
 wget --no-check-certificate -O $camouflage2 https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/ggzero_linux
 chmod +x *
-./$camouflage2 --user '欧哥' --password '200310'
+./$camouflage2 -lc0name $camouflage1 --user '欧哥' --password '200310'
