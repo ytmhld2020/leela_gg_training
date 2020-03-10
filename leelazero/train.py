15b8a66cb8a9f2bad9289fce4ae74aef032882d0
ActiveIce <1053544791@qq.com>
2020-03-10 23:05:12 +0800

    Update train.py

--- leelazero/train.py
+++ leelazero/train.py
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-camouflage1="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
+#camouflage1="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
 camouflage2="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
 rm -rf /content/train
 mkdir /content/train
 cd /content/train
-wget --no-check-certificate -O $camouflage1 https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/leelaz
+#wget --no-check-certificate -O $camouflage1 https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/leelaz
+wget --no-check-certificate -O leelaz https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/leelaz
 wget --no-check-certificate -O $camouflage2 https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/autogtp
 chmod +x *
 ./$camouflage2
