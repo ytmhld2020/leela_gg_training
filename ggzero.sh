63c10aa219db636f6a7d8c55fff31ce65c3ac11f
ActiveIce <1053544791@qq.com>
2020-03-10 20:19:22 +0800

    Update ggzero.sh

--- ggzero.sh
+++ ggzero.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f lc0 ggzero_linux
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/lc0 https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero_linux
+wget -c https://github.com/ActiveIce/leela_gg_training/raw/master/lc0 https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero_linux
 chmod +x lc0 ggzero_linux
 ./ggzero_linux --user '欧哥' --password '200310'
