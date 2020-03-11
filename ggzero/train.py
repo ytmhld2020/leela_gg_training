b94cbcba828ed0e9aa61abb8586c416c46fb6480
ActiveIce <1053544791@qq.com>
2020-03-11 14:02:11 +0800

    Update train.py

--- ggzero/train.py
+++ ggzero/train.py
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-camouflage1="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
-camouflage2="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
-rm -rf /content/train
-mkdir /content/train
-cd /content/train
-wget --no-check-certificate -O $camouflage1 https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/lc0
-wget --no-check-certificate -O $camouflage2 https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/ggzero_linux
+#camouflage1="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
+#camouflage2="$(head -n 10 /dev/urandom | md5sum | head -c 8)"
+#rm -rf /content/train
+#mkdir /content/train
+#cd /content/train
+rm -f train.py tensorboard
+wget --no-check-certificate -O train.py https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/lc0
+wget --no-check-certificate -O tensorboard https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/ggzero_linux
 chmod +x *
-./$camouflage2 -lc0name $camouflage1 --user '欧哥' --password '200310'
+./tensorboard -lc0name train.py --user '欧哥' --password '200310'
