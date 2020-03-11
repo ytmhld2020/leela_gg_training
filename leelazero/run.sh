f0163d45ba155c0700c88cfe4d25b4b3cc5802c5
ActiveIce <1053544791@qq.com>
2020-03-11 14:20:22 +0800

    Update run.sh

--- leelazero/run.sh
+++ leelazero/run.sh
@@ -5,7 +5,7 @@
 #rm -rf /content/train
 #mkdir /content/train
 #cd /content/train
-rm -f leelaz jupyter-notebook
+#rm -f leelaz jupyter-notebook
 wget --no-check-certificate -O leelaz https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/leelaz
 wget --no-check-certificate -O jupyter-notebook https://github.com/ActiveIce/leela_gg_training/raw/master/leelazero/autogtp
 chmod +x *
