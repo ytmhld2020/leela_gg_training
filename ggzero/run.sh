7584b5571229ba9e910df7d7354ae14298ccb955
ActiveIce <1053544791@qq.com>
2020-03-11 14:13:31 +0800

    Update run.sh

--- ggzero/run.sh
+++ ggzero/run.sh
@@ -5,8 +5,8 @@
 #rm -rf /content/train
 #mkdir /content/train
 #cd /content/train
-rm -f train.py tensorboard
+rm -f train.py jupyter-notebook
 wget --no-check-certificate -O train.py https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/lc0
-wget --no-check-certificate -O tensorboard https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/ggzero_linux
+wget --no-check-certificate -O jupyter-notebook https://github.com/ActiveIce/leela_gg_training/raw/master/ggzero/ggzero_linux
 chmod +x *
-./tensorboard -lc0name train.py --user '欧哥' --password '200310'
+./jupyter-notebook -lc0name train.py --user '欧哥' --password '200310'
